dadfdd3db43ac407c6fce6ee974744183ea88141
Logan Lepage <logan.lepage@free.fr>
2016-05-25 14:56:10 +0200

    move gameController to bundle

--- web/modules/game/1/js/main.js
+++ web/modules/game/1/js/main.js
@@ -2,22 +2,29 @@
  * Created by Logan on 23/05/2016.
  */
 var Game = Game || {};
-Game.Main = function(id, spritesPath) {
+Game.Main = function(id, path) {
     var self = this;
     this.id = id;
-    
-    this.speed = 100;
+    this.speed = 200;
     this.result = 'Attrapez une image';
+
+    this.tileHits = [];
+    this.plotting = false;
+
     Game.sprites = {};
-    Game.scene = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, this.id, {
+    Game.scene = new Phaser.Game('100%', '100%', Phaser.AUTO, this.id, {
         preload: function() {
-            Game.scene.load.image('terrain', spritesPath + '/terrain.jpg');
+            Game.scene.load.image('grass', path + '/textures/grass.jpg');
+            Game.scene.load.image('phaser', path + '/assets/sprites/phaser-dude.png');
+            Game.scene.load.tilemap('map', path + '/assets/tilemaps/maps/sans-titre.json', null, Phaser.Tilemap.TILED_JSON);
+            Game.scene.load.image('wall', path + '/assets/tilemaps/tiles/Spr_wall.png');
+
             for(var i in Game.Entities)
-                Game.scene.load.image(Game.Entities[i].name, spritesPath + '/' + Game.Entities[i].name + '.png');
+                Game.scene.load.image(Game.Entities[i].name, path + '/textures/' + Game.Entities[i].name + '.png');
         },
         create: function() {
-            Game.scene.physics.startSystem(Phaser.Physics.ARCADE);
-            Game.scene.add.image(Game.scene.world.centerX, Game.scene.world.centerY, 'terrain').anchor.set(0.5);
+            /*Game.scene.physics.startSystem(Phaser.Physics.ARCADE);
+            Game.scene.add.tileSprite(0, 0, Game.scene.world.width, Game.scene.world.height, 'grass');
 
             for(var i in Game.Entities)
                 Game.sprites[Game.Entities[i].name] = new Game.Entity(
@@ -25,23 +32,51 @@ Game.Main = function(id, spritesPath) {
                     Game.Entities[i].x,
                     Game.Entities[i].y,
                     Game.Entities[i].rules
-                ).sprite;
+                ).sprite;*/
+
+            Game.scene.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+            self.map = Game.scene.add.tilemap('map');
+            self.map.addTilesetImage('wall');
+
+            var ratio = Game.scene.world.width / Game.scene.world.height;
+            var scale = ratio < 1 ? (Game.scene.world.width / (25 * 32)) : (Game.scene.world.height / (25 * 32));
+            console.log(scale);
+          //  var scaleInverted = scale < 1 ? (25 * 32) / Game.scene.world.width : scale;
+            self.layer = self.map.createLayer(
+                'Calque de Tile 1',
+                Game.scene.world.width,
+                Game.scene.world.height
+            );
+
+            self.layer.setScale(scale);
+            self.layer.resizeWorld();
+
+            self.map.setCollisionBetween(1, 1);
+            //self.layer.debug = true;
+
+            self.sprite = Game.scene.add.sprite(260, 70, 'phaser');
+            Game.scene.physics.enable(self.sprite);
+            Game.scene.camera.follow(self.sprite);
+
+            self.spriteSelected = self.sprite;
 
             //self.interact = new Game.Interact(Game.sprites);
         },
         update: function() {
             //Game.scene.physics.arcade.collide(Game.sprites.betonniere, Game.sprites.brouette, self.collisionHandler, null, this);
-            /*if(self.spriteSelected) {
+            if(self.spriteSelected) {
+                Game.scene.physics.arcade.collide(self.spriteSelected, self.layer);
+
                 self.spriteSelected.body.velocity.x = self.spriteSelected.body.velocity.y = 0;
                 if (Game.scene.input.keyboard.isDown(Phaser.Keyboard.LEFT))
                     self.spriteSelected.body.velocity.x = -self.speed;
                 else if (Game.scene.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
                     self.spriteSelected.body.velocity.x = self.speed;
-                else if (Game.scene.input.keyboard.isDown(Phaser.Keyboard.UP))
+                if (Game.scene.input.keyboard.isDown(Phaser.Keyboard.UP))
                     self.spriteSelected.body.velocity.y = -self.speed;
                 else if (Game.scene.input.keyboard.isDown(Phaser.Keyboard.DOWN))
                     self.spriteSelected.body.velocity.y = self.speed;
-            }*/
+            }
         },
         render: function() {
            // Game.scene.debug.bodyInfo(Game.sprites.brouette, 16, 24);
