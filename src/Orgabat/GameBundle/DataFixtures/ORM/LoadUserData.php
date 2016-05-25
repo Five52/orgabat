9eeca6ee7d4ee7e9c19825aa1ca1338884e6caaa
Thomas Fortin <21410746@etu.unicaen.fr>
2016-05-25 14:36:19 +0200

    Création de DataFixtures pour tester les utilisateurs

--- src/Orgabat/GameBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Orgabat/GameBundle/DataFixtures/ORM/LoadUserData.php
@@ -10,29 +10,24 @@ class LoadUserData implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // Liste des noms de tags à ajouter
-        $firstNames = array(
-            'Jacques',
-            'Marcel',
-            'Thérèse'
-        );
+        $user1 = new User();
+        $user1->setFirstName('Jacques');
+        $user1->setLastName('Ouille');
+        $user1->setPassword('jacques');
 
-        $lastNames = array(
-            'Ouille',
-            'Osef',
-            'Rèserèse'
-        );
+        $user2 = new User();
+        $user2->setFirstName('Marcel');
+        $user2->setLastName('Osef');
+        $user2->setPassword('marcel');
 
-        
+        $user3 = new User();
+        $user3->setFirstName('Thérèse');
+        $user3->setLastName('Rèserèse');
+        $user3->setPassword('therese');
 
-        foreach ($names as $name) {
-            // On crée le tag
-            $category = new Tag();
-            $category->setName($name);
-
-            // On la persiste
-            $manager->persist($category);
-        }
+        $manager->persist($user1);
+        $manager->persist($user2);
+        $manager->persist($user3);
 
         // On déclenche l'enregistrement de tous les tags
         $manager->flush();
