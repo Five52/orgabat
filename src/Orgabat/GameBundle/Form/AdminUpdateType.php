51bfa36f9364aaca106ea746bf3138c478c99524
yboyer <bhyw180@gmail.com>
2017-01-12 17:46:23 +0100

    Fix class name

--- src/Orgabat/GameBundle/Form/AdminUpdateType.php
+++ src/Orgabat/GameBundle/Form/AdminUpdateType.php
@@ -19,7 +19,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class adminUpdateType extends AbstractType
+class AdminUpdateType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
